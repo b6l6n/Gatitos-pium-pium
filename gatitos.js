07706bac675a547d9bf3b7c63a8606d35a04b967
Belen L <belenjune@gmail.com>
2025-02-06 14:25:07 +0100

    empiezo script

--- gatitos.js
+++ gatitos.js
@@ -4,4 +4,20 @@ const canvas = document.getElementById('canvas');
 const img = canvas.getContext('2d');
 
 
-//GATITO
+//GATITOS
+
+const gatito = {
+    x: canvas.width / 2 - 25,
+    y: canvas.height - 100,7
+    width: 50,
+    height: 50,
+    color: 'rgb(172, 108, 26)'
+    speed: 50, //destancia de movimiento con click
+}
+    
+
+//ENEMIGOS
+ 
+const enemigo = [];
+const numeroEnemigos = 5;
+
