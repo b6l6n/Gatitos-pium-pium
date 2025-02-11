a259a87599811cd17bf216c3e9d0412670396d35
Belen L <belenjune@gmail.com>
2025-02-11 14:26:29 +0100

    comida y score

--- gatitos.js
+++ gatitos.js
@@ -1,7 +1,9 @@
 //CONFIGURACION INICIAL
 
 const canvas = document.getElementById('canvas');
-const img = canvas.getContext('2d');
+const ctx= canvas.getContext('2d');
+const canvasWidth= canvas.width;
+const canvasHeight = canvas.height;
 
 
 //GATITOS
@@ -14,9 +16,16 @@ const gatito = {
     speed: 50, //destancia de movimiento con click
 }
     
+//COMIDA/PUNTUACION
+const peces = {
+    x: Math.random() * (canvasWidth - 20),
+    y: Math.random() * (canvasHeight - 200),
+    width: 20,
+    height: 20,
+    color: 'orange',
+};
 
 //ENEMIGOS
- 
 const enemigo = [];
 const numeroEnemigos = 5;
 for(let i = 0; i < numeroEnemigos; i++){
@@ -25,10 +34,13 @@ for(let i = 0; i < numeroEnemigos; i++){
         y: Math.random()*(canvas.height / 2),
         width: 40,
         height: 40,
-        color: 'pink',
-        speed: Math.random() * 3 + 1,
+        color: 'black',
+        speed: Math.random() * 3 + 1, //velocidad aleatoria
+        laser: [],//array laser disparados
+        laserSpeed: Math.random() * 3 + 2 //velocidad laser
     });
 }
+
 
 //PUNTUACION
 let score = 0;
@@ -60,5 +72,22 @@ function moverEnemigos(){
     }
 };
 
+//DISPARAR LASER
+if (Math.random() < 0.02){
+    enemigo.toLocaleString.push({
+        x: enemigo.x + enemigo.width, / 2 - 2.5,
+        y: enemigo.y + enemigo.height,
+    });
+}
+
+//MOVER LASERES
+enemigo.laser.forEach((laser, index)=> {
+    laser.y += enemigo.laserSpeed;
+ 
+//ELIMINAR LASER SI SALE DE CANVAS
+    if (laser.y > canvasHeight) {
+        enemigo.laser.splice(index, 1);
+    }
+});
 
 
